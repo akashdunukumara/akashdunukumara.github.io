ee1f032b47d771a35f2e6d13afb554c9e743be84
akashdunukumara <akashdunukumara@gmail.com>
2025-08-01 14:42:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thanks for reaching out! We’ll get back to you soon.');
-  this.reset();
-});
